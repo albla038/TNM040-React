630ef97f872db40775d1b21fe7c6ef85995059dd
Albert0002 <albert.lannerstrom@gmail.com>
2023-09-11 15:00:25 +0200

    Step 6 done. Conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,15 @@ function App() {
                               .sort((b, c) => c.area - b.area)
                               .slice(0, 15)                        //Best practice för chained functions?
 
+  const maxArea = sortedList[0].area
+
   return (
     <div>
-      {sortedList.map((d) => (
-        <CountryInfo key={d.cca3} data={d}/>
+      {sortedList.map((d, i) => (
+        <CountryInfo key={d.cca3} data={d} widthRatio={(d.area/maxArea)*100*4 + "%"} detailed={i < 5}/>
       ))}
     </div>
+    
   )
 }
 
--- src/CountryInfo.jsx
+++ src/CountryInfo.jsx
@@ -1,10 +1,30 @@
 
 
-const CountryInfo = ({data}) => { //varför brackets men inte på props? Svar: om man skriver props ska det ej vara brackets. props är då parent till hela objektet
+const CountryInfo = ({data, widthRatio, detailed}) => { //varför brackets men inte på props? Svar: om man skriver props ska det ej vara brackets. props är då parent till hela objektet
     return (
-        <p>
-            <span className="country">{data.name.common} </span>{data.area} km<sup>2</sup>
-        </p>
+        <div>
+            <p>
+                <span className="country">{data.name.common} </span>{data.area} km<sup>2</sup>
+            </p>
+            {detailed == true && 
+                <div>
+                    <p>
+                        Capital: {data.capital[0]}
+                    </p>
+                    <p>
+                        Region: {data.subregion}
+                    </p>
+                </div>
+            }
+            <div 
+                style={{
+                    width: widthRatio,
+                    height: "0.5em",
+                    display: "inline-block",
+                    backgroundColor: "blue"
+                }}
+            />
+        </div>
     )
 }
 
