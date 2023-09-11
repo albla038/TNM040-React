d6a1d27eacc7c19af274055435be3b6a462faf41
Albert0002 <albert.lannerstrom@gmail.com>
2023-09-11 13:49:44 +0200

    Step 4 done. Filtering, sorting & slicing data set

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,19 @@ import CountryInfo from './CountryInfo.jsx'
 
 function App() {
 
-  console.log("Hello world!")
-  console.log(countries)
+  //console.log("Hello world!")
+  //console.log(countries)
+
+  const sortedList = countries.filter((a) => a.name.common != "Antarctica")
+                              .sort((b, c) => c.area - b.area)
+                              .slice(0, 15)                        //Best practice för chained functions?
 
   return (
-      <div>
-        <CountryInfo data={countries[0]}/>
-      </div>
+    <div>
+      {sortedList.map((d) => (
+        <CountryInfo key={d.cca3} data={d}/>
+      ))}
+    </div>
   )
 }
 
--- src/CountryInfo.jsx
+++ src/CountryInfo.jsx
@@ -1,12 +1,10 @@
 
 
-const CountryInfo = ({data}) => { //varför brackets men inte på props?
+const CountryInfo = ({data}) => { //varför brackets men inte på props? Svar: om man skriver props ska det ej vara brackets. props är då parent till hela objektet
     return (
-        <>
         <p>
             <span className="country">{data.name.common} </span>{data.area} km<sup>2</sup>
         </p>
-        </>
     )
 }
 
